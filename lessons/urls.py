c6271963d0f7a6c7b732a08e6813e0ef27216e67
anti <pixell_tm@bk.ru>
2024-01-06 14:31:18 +0300

    new

--- lessons/urls.py
+++ lessons/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('lessons/', views.lesson_list),
     path('code/', views.code_python),
-    path('code/execute_python_code/', views.execute_python_code)
+    path('code/execute_python_code/', views.execute_python_code),
 ]
