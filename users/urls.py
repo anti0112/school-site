83a10f1f4efc9863144d896bdc58c2656ea86f79
anti <pixell_tm@bk.ru>
2024-01-11 01:31:13 +0300

    front to login

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include, re_path
-
+from .views import LoginView, RegisterView, LogoutView
 urlpatterns = [
     path('auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken'))
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,57 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class LoginView(View):
+    template_name = 'auth.html'
+
+    def get(self, request, *args, **kwargs):
+        form = AuthenticationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Добро пожаловать, {username}!')
+                return redirect('http://localhost:8000/code/')  # Укажите свой URL для перенаправления после успешного входа
+            else:
+                messages.error(request, 'Неверные имя пользователя или пароль.')
+        return render(request, self.template_name, {'form': form})
 
 
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class RegisterView(View):
+    template_name = 'auth.html'
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Регистрация прошла успешно!')
+            return redirect('login/')  # Укажите свой URL для перенаправления после успешной регистрации
+        return render(request, self.template_name, {'form': form})
+
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        messages.info(request, 'Вы успешно вышли из системы.')
+        return redirect('/login')  # Укажите свой URL для перенаправления после выхода
